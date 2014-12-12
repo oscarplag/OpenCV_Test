377ed041dce5937930d2344d06f9c137515c79d9
Oscar Plag <oscar.plag@perkinelmer.com>
2014-12-12 09:23:01 -0800

    added SIFT feature matching

--- OpenCV_Test/OpenCV_Test.cpp
+++ OpenCV_Test/OpenCV_Test.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "stdafx.h"
+#include <Windows.h>
 #include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
@@ -33,6 +34,9 @@ int BilatCanny();
 int BilatCannyGPU();
 int HomographyTest();
 
+void MatchFeaturesSurf(Mat& img_object, Mat& img_scene);
+void MatchFeaturesSift(Mat& img_object, Mat& img_scene);
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -43,22 +47,44 @@ int _tmain(int argc, _TCHAR* argv[])
 
 int HomographyTest()
 {
+	::LARGE_INTEGER freq,t1,t2,t3,t4;
+
 	Mat img_object = imread( "Corrected.png", CV_LOAD_IMAGE_GRAYSCALE );
 	resize(img_object, img_object, Size(img_object.cols/4, img_object.rows/4));
 
 	Mat img_scene_orig = imread( "CorrectedRotated2.png", CV_LOAD_IMAGE_GRAYSCALE );
 	resize(img_scene_orig, img_scene_orig, Size(img_scene_orig.cols/4, img_scene_orig.rows/4));
 	Mat img_scene;
-	bilateralFilter ( img_scene_orig, img_scene, 10, 20, 5 );
+	bilateralFilter(img_scene_orig, img_scene, 5, 10, 3 );
+
+	Mat descriptors_object, descriptors_scene;
 
 	if( !img_object.data || !img_scene.data )
-	{ std::cout<< " --(!) Error reading images " << std::endl; return -1; }
+	{ std::cout<< " --(!) Error reading images " << std::endl; return -1; }       
+	
 
+	::QueryPerformanceFrequency(&freq);
+
+	::QueryPerformanceCounter(&t1);
+	MatchFeaturesSurf(img_object,img_scene);
+	::QueryPerformanceCounter(&t2);
+	::QueryPerformanceCounter(&t3);
+	MatchFeaturesSift(img_object,img_scene);
+	::QueryPerformanceCounter(&t4);
+
+	double elapsedTime = (t2.QuadPart - t1.QuadPart) * 1000.0/ freq.QuadPart;
+	double elapsedTime2 = (t4.QuadPart - t3.QuadPart) * 1000.0/ freq.QuadPart;
+
+	waitKey(0);
+	return 0;
+}
+
+void MatchFeaturesSurf(Mat& img_object, Mat& img_scene)
+{
 	//-- Step 1: Detect the keypoints using SURF Detector
 	int minHessian = 400;
 	
 	SurfFeatureDetector detector( minHessian );
-
 	std::vector<KeyPoint> keypoints_object, keypoints_scene;
 
 	detector.detect( img_object, keypoints_object );
@@ -132,10 +158,90 @@ int HomographyTest()
 	line( img_matches, scene_corners[3] + Point2f( img_object.cols, 0), scene_corners[0] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
 
 	//-- Show detected matches
-	imshow( "Good Matches & Object detection", img_matches );
+	namedWindow("Surf Matches",WINDOW_AUTOSIZE);
+	imshow( "Surf Matches", img_matches );
+}
 
-	waitKey(0);
-	return 0;
+void MatchFeaturesSift(Mat& img_object, Mat& img_scene)
+{
+	//-- Step 1: Detect the keypoints using SURF Detector
+	SiftFeatureDetector* detector = new SiftFeatureDetector();
+	
+	std::vector<KeyPoint> keypoints_object, keypoints_scene;
+
+	detector->detect( img_object, keypoints_object );
+	detector->detect( img_scene, keypoints_scene );
+
+	//-- Step 2: Calculate descriptors (feature vectors)
+	SiftDescriptorExtractor extractor;
+
+	Mat descriptors_object, descriptors_scene;
+
+	extractor.compute( img_object, keypoints_object, descriptors_object );
+	extractor.compute( img_scene, keypoints_scene, descriptors_scene );
+
+	//-- Step 3: Matching descriptor vectors using FLANN matcher
+	FlannBasedMatcher matcher;
+	std::vector< DMatch > matches;
+	matcher.match( descriptors_object, descriptors_scene, matches );
+
+	double max_dist = 0; double min_dist = 100;
+
+	//-- Quick calculation of max and min distances between keypoints
+	for( int i = 0; i < descriptors_object.rows; i++ )
+	{ double dist = matches[i].distance;
+	if( dist < min_dist ) min_dist = dist;
+	if( dist > max_dist ) max_dist = dist;
+	}
+
+	//min_dist = 0.05;
+
+	printf("-- Max dist : %f \n", max_dist );
+	printf("-- Min dist : %f \n", min_dist );
+
+	//-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
+	std::vector< DMatch > good_matches;
+
+	for( int i = 0; i < descriptors_object.rows; i++ )
+	{ if( matches[i].distance < 3*min_dist )
+		{ good_matches.push_back( matches[i]); }
+	}
+
+	Mat img_matches;
+	drawMatches( img_object, keypoints_object, img_scene, keypoints_scene,
+				good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+				vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+
+	//-- Localize the object
+	std::vector<Point2f> obj;
+	std::vector<Point2f> scene;
+
+	for( int i = 0; i < good_matches.size(); i++ )
+	{
+		//-- Get the keypoints from the good matches
+		obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
+		scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
+	}
+
+	Mat H = findHomography( obj, scene, CV_RANSAC );
+
+	//-- Get the corners from the image_1 ( the object to be "detected" )
+	std::vector<Point2f> obj_corners(4);
+	obj_corners[0] = cvPoint(0,0); obj_corners[1] = cvPoint( img_object.cols, 0 );
+	obj_corners[2] = cvPoint( img_object.cols, img_object.rows ); obj_corners[3] = cvPoint( 0, img_object.rows );
+	std::vector<Point2f> scene_corners(4);
+
+	perspectiveTransform( obj_corners, scene_corners, H);
+
+	//-- Draw lines between the corners (the mapped object in the scene - image_2 )
+	line( img_matches, scene_corners[0] + Point2f( img_object.cols, 0), scene_corners[1] + Point2f( img_object.cols, 0), Scalar(0, 255, 0), 4 );
+	line( img_matches, scene_corners[1] + Point2f( img_object.cols, 0), scene_corners[2] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+	line( img_matches, scene_corners[2] + Point2f( img_object.cols, 0), scene_corners[3] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+	line( img_matches, scene_corners[3] + Point2f( img_object.cols, 0), scene_corners[0] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+
+	//-- Show detected matches
+	namedWindow("Sift Matches",WINDOW_AUTOSIZE);
+	imshow( "Sift Matches", img_matches );
 }
 
 int BilatCannyGPU()
