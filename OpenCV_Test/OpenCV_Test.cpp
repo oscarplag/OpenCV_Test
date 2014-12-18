332594936fd4731c89f902a60ba93627f67c15b4
Oscar Plag <oscar.plag@perkinelmer.com>
2014-12-17 18:00:48 -0800

    added cpu orb implementation

--- OpenCV_Test/OpenCV_Test.cpp
+++ OpenCV_Test/OpenCV_Test.cpp
@@ -39,6 +39,7 @@ int HomographyTest();
 
 void MatchFeaturesSurf(Mat& img_object, Mat& img_scene, Mat& img_matches);
 void MatchFeaturesSift(Mat& img_object, Mat& img_scene, Mat& img_matches);
+void MatchFeaturesORB(Mat& img_object, Mat& img_scene, Mat& img_matches);
 void MatchFeaturesORB_GPU(gpu::GpuMat& img_object, gpu::GpuMat& img_scene, Mat& img_matches);
 int HomographyGPU();
 
@@ -163,7 +164,7 @@ int HomographyGPU()
 
 int HomographyTest()
 {
-	::LARGE_INTEGER freq,t1,t2,t3,t4;	
+	::LARGE_INTEGER freq,t1,t2,t3,t4,t5;	
 	Mat img_object = imread( "Corrected.png", CV_LOAD_IMAGE_GRAYSCALE );	
 	Mat img_scene_orig = imread( "CorrectedRotated2.png", CV_LOAD_IMAGE_GRAYSCALE );
 #ifdef RESIZE
@@ -187,7 +188,7 @@ int HomographyTest()
 	if( !img_object.data || !img_scene.data )
 	{ std::cout<< " --(!) Error reading images " << std::endl; return -1; }       
 	
-	Mat matches_sift, matches_surf, matches_orb;
+	Mat matches_sift, matches_surf, matches_orb, matches_orb_GPU;
 
 	::QueryPerformanceFrequency(&freq);
 
@@ -195,23 +196,24 @@ int HomographyTest()
 	MatchFeaturesSurf(img_object,img_scene,matches_surf);
 	::QueryPerformanceCounter(&t2);
 #ifdef GPU
-	MatchFeaturesORB_GPU(img_object_GPU,img_scene_GPU,matches_orb);
+	MatchFeaturesORB_GPU(img_object_GPU,img_scene_GPU,matches_orb_GPU);
 #endif
 	::QueryPerformanceCounter(&t3);
-	MatchFeaturesSift(img_object,img_scene,matches_sift);
+	MatchFeaturesORB(img_object,img_scene,matches_orb);
 	::QueryPerformanceCounter(&t4);
+	MatchFeaturesSift(img_object,img_scene,matches_sift);
+	::QueryPerformanceCounter(&t5);
 
 	double elapsedTime = (t2.QuadPart - t1.QuadPart) * 1000.0/ freq.QuadPart;
-#ifdef GPU
-	double elapsedTime2 = (t3.QuadPart - t2.QuadPart) * 1000.0/ freq.QuadPart;
-#endif
-	double elapsedTime3 = (t4.QuadPart - t3.QuadPart) * 1000.0/ freq.QuadPart;
-
 	printf("Surf took: %fms\n",elapsedTime);
 #ifdef GPU
+	double elapsedTime2 = (t3.QuadPart - t2.QuadPart) * 1000.0/ freq.QuadPart;
 	printf("Orb (GPU) took: %fms\n",elapsedTime2);
 #endif
-	printf("Sift took: %fms\n",elapsedTime3);
+	double elapsedTime3 = (t4.QuadPart - t3.QuadPart) * 1000.0/ freq.QuadPart;
+	printf("Orb (CPU) took: %fms\n",elapsedTime3);
+	double elapsedTime4 = (t5.QuadPart - t4.QuadPart) * 1000.0/ freq.QuadPart;
+	printf("Sift took: %fms\n",elapsedTime4);
 
 #ifdef DISPLAY_IMAGES
 	namedWindow("Surf Matches",WINDOW_AUTOSIZE);
@@ -221,8 +223,10 @@ int HomographyTest()
 	imshow( "Sift Matches", matches_sift );
 #ifdef GPU
 	namedWindow("Orb Matches (GPU)",WINDOW_AUTOSIZE);
-	imshow("Orb Matches (GPU)", matches_orb );
+	imshow("Orb Matches (GPU)", matches_orb_GPU );
 #endif
+	namedWindow("Orb Matches (CPU)",WINDOW_AUTOSIZE);
+	imshow("Orb Matches (CPU)", matches_orb );
 #endif
 
 	waitKey(0);
@@ -382,6 +386,82 @@ void MatchFeaturesSift(Mat& img_object, Mat& img_scene, Mat& img_matches)
 	line( img_matches, scene_corners[1] + Point2f( img_object.cols, 0), scene_corners[2] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
 	line( img_matches, scene_corners[2] + Point2f( img_object.cols, 0), scene_corners[3] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
 	line( img_matches, scene_corners[3] + Point2f( img_object.cols, 0), scene_corners[0] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+}
+void MatchFeaturesORB(Mat& img_object, Mat& img_scene, Mat& img_matches)
+{
+	//-- Step 1: Detect the keypoints using SURF Detector
+	::OrbFeatureDetector detector(2000);
+	
+	std::vector<KeyPoint> keypoints_object, keypoints_scene;
+
+	detector.detect( img_object, keypoints_object );
+	detector.detect( img_scene, keypoints_scene );
+
+	//-- Step 2: Calculate descriptors (feature vectors)
+	::OrbDescriptorExtractor extractor;
+
+	Mat descriptors_object, descriptors_scene;
+
+	extractor.compute( img_object, keypoints_object, descriptors_object );
+	extractor.compute( img_scene, keypoints_scene, descriptors_scene );
+
+	//-- Step 3: Matching descriptor vectors using FLANN matcher
+	::BFMatcher matcher;
+	std::vector< DMatch > matches;
+	matcher.match( descriptors_object, descriptors_scene, matches );
+
+	double max_dist = 0; double min_dist = 100;
+
+	//-- Quick calculation of max and min distances between keypoints
+	for( int i = 0; i < descriptors_object.rows; i++ )
+	{ double dist = matches[i].distance;
+	if( dist < min_dist ) min_dist = dist;
+	if( dist > max_dist ) max_dist = dist;
+	}
+
+	//min_dist = 0.05;
+
+	//printf("-- Max dist : %f \n", max_dist );
+	//printf("-- Min dist : %f \n", min_dist );
+
+	//-- Draw only "good" matches (i.e. whose distance is less than 3*min_dist )
+	std::vector< DMatch > good_matches;
+
+	for( int i = 0; i < descriptors_object.rows; i++ )
+	{ if( matches[i].distance < 3*min_dist )
+		{ good_matches.push_back( matches[i]); }
+	}
+
+	drawMatches( img_object, keypoints_object, img_scene, keypoints_scene,
+				good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+				vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+
+	//-- Localize the object
+	std::vector<Point2f> obj;
+	std::vector<Point2f> scene;
+
+	for( int i = 0; i < good_matches.size(); i++ )
+	{
+		//-- Get the keypoints from the good matches
+		obj.push_back( keypoints_object[ good_matches[i].queryIdx ].pt );
+		scene.push_back( keypoints_scene[ good_matches[i].trainIdx ].pt );
+	}
+
+	Mat H = findHomography( obj, scene, CV_RANSAC );
+
+	//-- Get the corners from the image_1 ( the object to be "detected" )
+	std::vector<Point2f> obj_corners(4);
+	obj_corners[0] = cvPoint(0,0); obj_corners[1] = cvPoint( img_object.cols, 0 );
+	obj_corners[2] = cvPoint( img_object.cols, img_object.rows ); obj_corners[3] = cvPoint( 0, img_object.rows );
+	std::vector<Point2f> scene_corners(4);
+
+	perspectiveTransform( obj_corners, scene_corners, H);
+
+	//-- Draw lines between the corners (the mapped object in the scene - image_2 )
+	line( img_matches, scene_corners[0] + Point2f( img_object.cols, 0), scene_corners[1] + Point2f( img_object.cols, 0), Scalar(0, 255, 0), 4 );
+	line( img_matches, scene_corners[1] + Point2f( img_object.cols, 0), scene_corners[2] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+	line( img_matches, scene_corners[2] + Point2f( img_object.cols, 0), scene_corners[3] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );
+	line( img_matches, scene_corners[3] + Point2f( img_object.cols, 0), scene_corners[0] + Point2f( img_object.cols, 0), Scalar( 0, 255, 0), 4 );	
 }
 
 void MatchFeaturesORB_GPU(gpu::GpuMat& img_object, gpu::GpuMat& img_scene, Mat& img_matches)
